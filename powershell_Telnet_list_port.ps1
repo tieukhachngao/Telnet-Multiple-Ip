54b5af22a2f6cc0445ab10ffab632b1b518c0b69
pham quang loc <tieukhachngao75@gmail.com>
2019-10-02 14:34:40 +0700

    Upload

---
+++ powershell_Telnet_list_port.ps1
@@ -0,0 +1,27 @@
+
+$TopIP = @("111.65.250.2","222.255.239.80")
+$PortList = @(21,22)
+
+$TopIP | ForEach-Object {
+    
+        $IP = Test-NetConnection $_ | Select ComputerName,TcpTestSucceeded,RemoteAddress,RemotePort
+        $PortList | ForEach-Object{
+
+        $TcpTestSucceeded = (Test-NetConnection $Domain.ComputerName -Port $_ -ErrorAction SilentlyContinue -WarningAction SilentlyContinue).TcpTestSucceeded
+
+        $RemoteAddress = (Test-NetConnection $Domain.ComputerName -Port $_ -ErrorAction SilentlyContinue -WarningAction SilentlyContinue).RemoteAddress
+        
+        $RemotePort = (Test-NetConnection $Domain.ComputerName -Port $_ -ErrorAction SilentlyContinue -WarningAction SilentlyContinue).RemotePort
+         
+        $date = Get-Date
+
+        if($TcpTestSucceeded -eq "True"){
+             $TcpTestSucceeded = "Success"
+
+        }else{
+             $TcpTestSucceeded = "Failed"
+             Write-Host "Telnet status  $TcpTestSucceeded  - To IP $RemoteAddress with port $RemotePort : $date" -ForegroundColor Red
+        }   
+    }
+    Write-Host "Telnet status  $TcpTestSucceeded - To IP $RemoteAddress with port $RemotePort : $date" -ForegroundColor Green
+} | FT
